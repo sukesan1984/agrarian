e95bfa74bd01d3176074ef785d40a74a4906d6cb
sukesan1984 <takamikousuke@gmail.com>
2015-07-07 23:37:13 +0900

    rm controller tests

--- test/controllers/area_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AreaControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/road_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RoadControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
