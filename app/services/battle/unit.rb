79206cdff2f18197f5d0b390b60b753238c79ea4
sukesan1984 <takamikousuke@gmail.com>
2015-07-07 22:30:54 +0900

    ダメージ計算をdqと同じようにしてみた。

--- app/services/battle/unit.rb
+++ app/services/battle/unit.rb
@@ -1,6 +1,6 @@
 class Battle::Unit
   attr_accessor :done_action
-  attr_reader :name, :hp, :is_dead
+  attr_reader :name, :hp, :is_dead, :defense, :attack
   # battlize.name
   # battlize.attack
   # battlize.defense
@@ -22,10 +22,16 @@ class Battle::Unit
     end
   end
 
+  # このユニットがターンで何をするか決める。
   def get_action(party)
     unit = party.get_attackable_unit
-    unit.take_damage(@attack)
-    return Battle::Action.new(self, unit, "ダメージを与えた", @attack);
+    target_defense = unit.defense
+    ave_damage = (@attack / 2.0 - target_defense / 4.0).ceil
+    range = -(ave_damage / 16.0).ceil..(ave_damage/16.0).ceil
+    randomize = Random.rand(range)
+    damage = ave_damage + randomize + 1
+    unit.take_damage(damage)
+    return Battle::Action.new(self, unit, "ダメージを与えた", damage)
   end
 
   def get_current_state
