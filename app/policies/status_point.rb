f561b928c641cc9393e4abb99d29756f642c88d3
sukesan1984 <takamikousuke@gmail.com>
2015-07-08 16:44:50 +0900

    最大値と0の間で現在値を取るStatusPointの実装

---
+++ app/policies/status_point.rb
@@ -0,0 +1,43 @@
+class StatusPoint
+  attr_reader :current, :max
+  def initialize(current, max)
+    @current = current
+    @max     = max
+  end
+
+  def -(other)
+    self.decrease(other)
+    return self
+  end
+
+  def decrease(value)
+    if(!value.is_a?(Integer))
+      return
+    end
+    self.set_current(@current - value)
+  end
+
+  def +(other)
+    self.increase(other)
+    return self
+  end
+
+  def increase(value)
+    if(!value.is_a?(Integer))
+      return
+    end
+    self.set_current(@current + value)
+  end
+
+  def set_current(value)
+    if(value > @max)
+      @current = @max
+      return
+    end
+    if(value < 0)
+      @current = 0
+      return
+    end
+    @current = value
+  end
+end
