d67706a39685a317d75e71dacd7e8ab116e8a047
sukesan1984 <takamikousuke@gmail.com>
2015-07-11 15:43:14 +0900

    森へと続く道を作る。

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,16 @@ Area.delete_all
 areas = Area.first_or_create([
   { id: 10001, area_type: 1, type_id: 1 }, # 始まりの街
   { id: 10002, area_type: 1, type_id: 2 }, # 次の街
-  { id: 20001, area_type: 2, type_id: 1 }  # 始まりの街と次の街をつなぐ街道
+  { id: 10003, area_type: 1, type_id: 3 }, # エンモルド
+  { id: 20001, area_type: 2, type_id: 1 }, # 始まりの街と次の街をつなぐ街道
+  { id: 20002, area_type: 2, type_id: 2 }  # 始まりの街と近くの森をつなぐ街道
 ])
 
 Road.delete_all
 roads = Road.first_or_create([
   { id: 1, name: "古くからある道", road_length: 5 },
-  { id: 2, name: "ドラゴノーム", road_length: 10 }
+  { id: 2, name: "森へと続く道", road_length: 3 },
+  { id: 3, name: "ドラゴノーム", road_length: 10 }
 ])
 
 AreaNode.delete_all
@@ -40,7 +43,11 @@ area_noads = AreaNode.first_or_create([
   { id: 200012, area_id: 20001, node_point: 2},
   { id: 200013, area_id: 20001, node_point: 3},
   { id: 200014, area_id: 20001, node_point: 4},
-  { id: 200015, area_id: 20001, node_point: 5}
+  { id: 200015, area_id: 20001, node_point: 5},
+  { id: 200021, area_id: 20002, node_point: 1},
+  { id: 200022, area_id: 20002, node_point: 2},
+  { id: 200023, area_id: 20002, node_point: 3},
+  { id: 100031, area_id: 10003, node_point: 1}
 ])
 
 Route.delete_all
@@ -48,7 +55,11 @@ routes = Route.first_or_create([
   { id: 1, area_node_id: 100011, connected_area_node_id: 200011 },
   { id: 2, area_node_id: 200011, connected_area_node_id: 100011 },
   { id: 3, area_node_id: 200015, connected_area_node_id: 100021 },
-  { id: 4, area_node_id: 100021, connected_area_node_id: 200015 }
+  { id: 4, area_node_id: 100021, connected_area_node_id: 200015 },
+  { id: 5, area_node_id: 200013, connected_area_node_id: 200021 },
+  { id: 6, area_node_id: 200021, connected_area_node_id: 200013 },
+  { id: 7, area_node_id: 200023, connected_area_node_id: 100031 },
+  { id: 8, area_node_id: 100031, connected_area_node_id: 200023 }
 ])
 
 Enemy.delete_all
