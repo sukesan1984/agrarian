51e719f638ce83970240ebfc17528cccbedf63b6
sukesan1984 <takamikousuke@gmail.com>
2015-07-12 23:11:25 +0900

    川の水も汲めるようにしたよ

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,12 +30,14 @@ roads = Road.first_or_create([
   { id: 1, name: "古くからある道", road_length: 5 },
   { id: 2, name: "森へと続く道", road_length: 3 },
   { id: 3, name: "ドラゴノーム", road_length: 10 },
-  { id: 4, name: "迷いの道", road_length: 4 }
+  { id: 4, name: "迷いの道", road_length: 4 },
+  { id: 5, name: "ドンブラコ峠", road_length: 4}
 ])
 
 NatureField.delete_all
 nature_fields = NatureField.first_or_create([
-  { id: 1, name: "近くの森「ヌージュ」", description: "森と呼べるのか果たして怪しいような小さな森。", resource_action_id: 1, resource_id: 1 }
+  { id: 1, name: "近くの森「ヌージュ」", description: "森と呼べるのか果たして怪しいような小さな森。", resource_action_id: 100001, resource_id: 1 },
+  { id: 2, name: "ローム川", description: "枯れそうな川。", resource_action_id: 100002, resource_id: 3}
 ])
 
 Dungeon.delete_all
@@ -45,18 +47,21 @@ dungeons = Dungeon.first_or_create([
 
 Harvest.delete_all
 harvests = Harvest.first_or_create([
-  { id: 1, name: "木を切る", description: "その辺りに生えている木を切る行為。やりすぎると自然破壊につながるので注意" }
+  { id: 1, name: "木を切る", description: "その辺りに生えている木を切る行為。やりすぎると自然破壊につながるので注意" },
+  { id: 2, name: "水を汲む", description: "水を汲む行為。雨がしばらく続かないと干ばつしてしまうかもしれない。" }
 ])
 
 Resource.delete_all
 resources = Resource.first_or_create([
   { id: 1, name: "ただの木",             item_id: 100001,  recover_count: 1, recover_interval: 60,   max_count: 100 },
-  { id: 2, name: "一般的なお店の木切れ", item_id: 100001, recover_count: 10, recover_interval: 1800, max_count: 100}
+  { id: 2, name: "一般的なお店の木切れ", item_id: 100001, recover_count: 10, recover_interval: 1800, max_count: 100},
+  { id: 3, name: "川の水", item_id: 100002, recover_count: 10, recover_interval: 60, max_count: 1000 }
 ])
 
 ResourceAction.delete_all
 resource_actions = ResourceAction.first_or_create([
-  { id: 1, action_type: 1, action_id: 1 }
+  { id: 100001, action_type: 1, action_id: 1 },
+  { id: 100002, action_type: 1, action_id: 2 }
 ])
 
 Shop.delete_all
@@ -80,7 +85,9 @@ areas = Area.first_or_create([
   { id: 20001, area_type: 2, type_id: 1 }, # 始まりの街と次の街をつなぐ街道
   { id: 20002, area_type: 2, type_id: 2 }, # 始まりの街と近くの森をつなぐ街道
   { id: 20004, area_type: 2, type_id: 4 }, # 森とゴブリンの洞窟をつなぐ街道
+  { id: 20005, area_type: 2, type_id: 5 }, # ジットとロームの川をつなぐ道
   { id: 30001, area_type: 3, type_id: 1 }, # 近くの森 
+  { id: 30002, area_type: 3, type_id: 2 }, # 川「ローム」
   { id: 40001, area_type: 4, type_id: 1 }  # ゴブリンの洞窟
 ])
 
@@ -101,7 +108,12 @@ area_noads = AreaNode.first_or_create([
   { id: 200042, area_id: 20004, node_point: 2},
   { id: 200043, area_id: 20004, node_point: 3},
   { id: 200044, area_id: 20004, node_point: 4},
+  { id: 200051, area_id: 20005, node_point: 1},
+  { id: 200052, area_id: 20005, node_point: 2},
+  { id: 200053, area_id: 20005, node_point: 3},
+  { id: 200054, area_id: 20005, node_point: 4},
   { id: 300011, area_id: 30001, node_point: 1},
+  { id: 300021, area_id: 30002, node_point: 1},
   { id: 400011, area_id: 40001, node_point: 1}
 ])
 
@@ -118,7 +130,11 @@ routes = Route.first_or_create([
   { id: 9,  area_node_id: 300011, connected_area_node_id: 200041 },
   { id: 10,  area_node_id: 200041, connected_area_node_id: 300011 },
   { id: 11,  area_node_id: 200044, connected_area_node_id: 400011 },
-  { id: 12, area_node_id: 400011, connected_area_node_id: 200044 }
+  { id: 12, area_node_id: 400011, connected_area_node_id: 200044 },
+  { id: 13, area_node_id: 100021, connected_area_node_id: 200051 },
+  { id: 14, area_node_id: 200051, connected_area_node_id: 100021 },
+  { id: 15, area_node_id: 200054, connected_area_node_id: 300021 },
+  { id: 16, area_node_id: 300021, connected_area_node_id: 200054 }
 ])
 
 Enemy.delete_all
@@ -129,5 +145,6 @@ enemies = Enemy.first_or_create([
 
 Item.delete_all
 items = Item.first_or_create([
-  { id: 100001, name: "木切れ", description: "木の切れ端。何の木から出たものかはよくわからない。", item_type: 1, item_type_id: 1 }
+  { id: 100001, name: "木切れ", description: "木の切れ端。何の木から出たものかはよくわからない。", item_type: 1, item_type_id: 1 },
+  { id: 100002, name: "ただの水", description: "水。川とかその辺の池から汲める。", item_type: 1, item_type_id: 2 },
 ])
