a61c2fea469445f8e6cdc96e57abf8a10df28ea4
sukesan1984 <takamikousuke@gmail.com>
2015-07-19 19:38:07 +0900

    user_equipmentsの構造を変更する。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719064313) do
+ActiveRecord::Schema.define(version: 20150719103705) do
 
   create_table "area_nodes", force: :cascade do |t|
     t.integer  "area_id",    limit: 4
@@ -229,14 +229,23 @@ ActiveRecord::Schema.define(version: 20150719064313) do
   add_index "user_encounter_enemies", ["player_id"], name: "index_user_encounter_enemies_on_player_id", using: :btree
 
   create_table "user_equipments", force: :cascade do |t|
-    t.integer  "player_id",    limit: 4
-    t.integer  "body_region",  limit: 4
-    t.integer  "user_item_id", limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "player_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "right_hand", limit: 4
+    t.integer  "left_hand",  limit: 4
+    t.integer  "both_hand",  limit: 4
+    t.integer  "body",       limit: 4
+    t.integer  "head",       limit: 4
+    t.integer  "leg",        limit: 4
+    t.integer  "neck",       limit: 4
+    t.integer  "belt",       limit: 4
+    t.integer  "amulet",     limit: 4
+    t.integer  "ring_a",     limit: 4
+    t.integer  "ring_b",     limit: 4
   end
 
-  add_index "user_equipments", ["player_id", "body_region"], name: "index_user_equipments_on_player_id_and_body_region", using: :btree
+  add_index "user_equipments", ["player_id"], name: "index_user_equipments_on_player_id_and_body_region", using: :btree
 
   create_table "user_items", force: :cascade do |t|
     t.integer  "player_id",  limit: 4
--- spec/models/user_equipment_spec.rb
+++ spec/models/user_equipment_spec.rb
@@ -2,16 +2,25 @@
 #
 # Table name: user_equipments
 #
-#  id           :integer          not null, primary key
-#  player_id    :integer
-#  body_region  :integer
-#  user_item_id :integer
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id         :integer          not null, primary key
+#  player_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  right_hand :integer
+#  left_hand  :integer
+#  both_hand  :integer
+#  body       :integer
+#  head       :integer
+#  leg        :integer
+#  neck       :integer
+#  belt       :integer
+#  amulet     :integer
+#  ring_a     :integer
+#  ring_b     :integer
 #
 # Indexes
 #
-#  index_user_equipments_on_player_id_and_body_region  (player_id,body_region)
+#  index_user_equipments_on_player_id_and_body_region  (player_id)
 #
 
 require 'rails_helper'
