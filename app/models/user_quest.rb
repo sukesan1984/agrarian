a7d2b075b2e33dd927f95ba14dd52308adc15ac3
sukesan1984 <takamikousuke@gmail.com>
2015-07-23 23:23:26 +0900

    クエストの受注所の作成

--- app/models/user_quest.rb
+++ app/models/user_quest.rb
@@ -15,4 +15,38 @@
 #
 
 class UserQuest < ActiveRecord::Base
+  belongs_to :quest
+
+  def self.find_or_create(player_id, quest_id)
+    user_quest = UserQuest.find_by(player_id: player_id, quest_id: quest_id)
+    if user_quest.nil?
+      user_quest = UserQuest.create(
+        player_id: player_id,
+        quest_id: quest_id,
+        status: 0
+      )
+    end
+    return user_quest
+  end
+
+  module Status
+    NotReceived = 0
+    Received = 1
+    NotReceivedReward = 2
+    ReceivedReward = 3
+  end
+
+  # 新しい報酬を受け取れるかどうか
+  def can_receive
+    return self.status == 0 || self.status == 3
+  end
+
+  def set_status_to_received 
+    change_status(Status::Received)
+  end
+
+  private 
+  def change_status(status)
+    self.status = status
+  end
 end
