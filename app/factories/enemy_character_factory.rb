36e92b778f7b4f6ce2b0822b470134c40187f31f
sukesan1984 <takamikousuke@gmail.com>
2015-07-27 21:56:56 +0900

    敵を倒した履歴を管理する

---
+++ app/factories/enemy_character_factory.rb
@@ -0,0 +1,18 @@
+class EnemyCharacterFactory
+  def initialize
+    @progress_cache = Hash.new
+  end
+
+  # enemyから生成する
+  def  build_by_enemy(enemy)
+    if @progress_cache.has_key?(enemy.id)
+      user_progress = @progress_cache[enemy.id]
+    else
+      user_progress = UserProgress.get_or_create(ProgressType::KillEnemy, enemy.id)
+      # cache
+      @progress_cache[enemy.id] = user_progress
+    end
+
+    return EnemyCharacter.new(enemy, user_progress)
+  end
+end
--- app/models/user_progress.rb
+++ app/models/user_progress.rb
@@ -11,4 +11,19 @@
 #
 
 class UserProgress < ActiveRecord::Base
+  def self. get_or_create(progress_type, progress_id)
+    user_progress = UserProgress.find_by(progress_type: progress_type, progress_id: progress_id)
+    if user_progress.nil?
+      user_progress = UserProgress.create(
+        progress_type: progress_type,
+        progress_id: progress_id,
+        count: 0
+      )
+    end
+    return user_progress
+  end
+end
+
+module ProgressType
+  KillEnemy = 1
 end
