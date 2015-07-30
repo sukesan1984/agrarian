6fd9bc1335e2c3d64a4868f3acd4f8233105b13d
MaxMEllon <mozi_kke@excite.co.jp>
2015-07-31 04:30:12 +0900

    [refacter] CodeClimateよりShopControllerの複雑度を解消

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,79 +1,74 @@
 class ShopController < ApplicationController
+  before_action :set_factories
+  before_action :set_shop
+  before_action :set_area_node
+  before_action :set_resource
+  before_action :set_player_character
+
   def index
-    shop_id = params[:id]
-    area_node_id = params[:area_node_id]
+    redirect_to player_input_path if @player_character.nil?
 
-    @shop = Shop.find_by(id: shop_id)
-
-    resource_service_factory = ResourceServiceFactory.new
-    # factory
-    equipment_service_factory = EquipmentServiceFactory.new
-    equipped_service_factory = EquippedServiceFactory.new(equipment_service_factory)
-    equipped_list_service_factory = EquippedListServiceFactory.new(equipped_service_factory)
-    player_character_factory = PlayerCharacterFactory.new(equipped_list_service_factory)
-    user_item_factory = UserItemFactory.new(equipped_list_service_factory)
-
-    # player
-    @player_character = player_character_factory.build_by_user_id(current_user.id)
-    if @player_character.nil?
-      redirect_to('/player/input')
-      return
-    end
+    user_item_factory = UserItemFactory.new(@equipped_list_service_factory)
 
     @showcases = []
     @shop.showcases.each do |showcase|
-      @showcases.push(Shop::ShowcaseService.new(resource_service_factory.build_by_target_id_and_resource(area_node_id, showcase.resource), area_node_id, showcase))
+      @showcases.push(Shop::ShowcaseService.new(@resource_service_factory.build_by_target_id_and_resource(@area_node.id, showcase.resource), @area_node.id, showcase))
     end
 
     @user_items = user_item_factory.build_unequipped_user_item_list_by_player_id(@player_character.id)
   end
 
-  # 購入する
   def buy
-    shop_id = params[:shop_id]
-    area_node_id = params[:area_node_id]
-    resource_id = params[:resource_id]
-
-    # factory
-    equipment_service_factory = EquipmentServiceFactory.new
-    equipped_service_factory = EquippedServiceFactory.new(equipment_service_factory)
-    equipped_list_service_factory = EquippedListServiceFactory.new(equipped_service_factory)
-    player_character_factory = PlayerCharacterFactory.new(equipped_list_service_factory)
-
     # TODO: validateクラスに寄せる
-    area_node = AreaNode.find_by(id: area_node_id)
-    @shop      = Shop.find_by(id: shop_id)
-    resource  = Resource.find_by(id: resource_id)
+    redirect_to root_path if @area_node.nil? || @resource.nil? || @shop.nil?
+    redirect_to player_input_path if @player_character.nil?
 
-    if area_node.nil? || resource.nil? || @shop.nil?
-      redirect_to('/')
-      return
-    end
+    showcase = Showcase.find_by(shop_id: @shop.id, resource_id: @resource.id)
+    fail 'showcase is nil' if showcase.nil?
 
-    player_character = player_character_factory.build_by_user_id(current_user.id)
+    resource_service = @resource_service_factory.build_by_target_id_and_resource(@area_node.id, @resource)
 
-    redirect_to('/player/input') if player_character.nil?
-
-    resource_service_factory = ResourceServiceFactory.new
-    resource_service = resource_service_factory.build_by_target_id_and_resource(area_node_id, resource)
-
-    item_service_factory = ItemServiceFactory.new(player_character)
+    item_service_factory = ItemServiceFactory.new(@player_character)
     item_service = item_service_factory.build_by_item_id(resource_service.item.id, 1)
 
-    fail 'shop is nil' if @shop.nil?
-    fail 'resource is nil' if resource.nil?
-
-    showcase = Showcase.find_by(shop_id: @shop.id, resource_id: resource.id)
-
-    resource_purchase_service = ResourceAction::ResourcePurchaseService.new(resource_service, item_service, player_character.player, showcase)
+    resource_purchase_service = ResourceAction::ResourcePurchaseService.new(resource_service, item_service, @player_character.player, showcase)
 
     @result = resource_purchase_service.execute
-    redirect_to('/') unless @result[:success]
+    redirect_to root_path unless @result[:success]
 
     @showcases = []
     @shop.showcases.each do |s|
-      @showcases.push(Shop::ShowcaseService.new(resource_service_factory.build_by_target_id_and_resource(area_node_id, s.resource), area_node_id, s))
+      @showcases.push(Shop::ShowcaseService.new(@resource_service_factory.build_by_target_id_and_resource(@area_node.id, s.resource), @area_node.id, s))
     end
+  end
+
+  private
+
+  def set_factories
+    @resource_service_factory      = ResourceServiceFactory.new
+    equipment_service_factory      = EquipmentServiceFactory.new
+    equipped_service_factory       = EquippedServiceFactory.new(equipment_service_factory)
+    @equipped_list_service_factory = EquippedListServiceFactory.new(equipped_service_factory)
+    @player_character_factory      = PlayerCharacterFactory.new(@equipped_list_service_factory)
+  end
+
+  def set_player_character
+    @player_character = @player_character_factory.build_by_user_id(current_user.id)
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:id]) if params[:id]
+    @shop = Shop.find(params[:shop_id]) if params[:shop_id]
+  end
+
+  def set_area_node
+    return unless params[:area_node_id]
+    @area_node = AreaNode.find(params[:area_node_id])
+  end
+
+  def set_resource
+    return unless params[:resource_id]
+    @resource = Resource.find(params[:resource_id])
   end
 end
 
