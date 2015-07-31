232f808a5748b18bf01af736f88a95b1df98a004
sukesan1984 <takamikousuke@gmail.com>
2015-07-31 18:06:03 +0900

    factoryをまとめる

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,34 +2,26 @@ class ItemController < ApplicationController
   before_action :authenticate_user!
   before_action :set_factories
   def index
+
     # player
     @player_character = @player_character_factory.build_by_user_id(current_user.id)
     redirect_to('/player/input') if @player_character.nil?
 
-    trait_factory = TraitFactory.new(@player_character, @soldier_character_facotry)
-    item_consumption_service_factory = ItemConsumptionServiceFactory.new(trait_factory)
-
     @user_items = UserItem.where('player_id = ?', @player_character.player.id).select { |user_item| user_item.count > 0 }
-    @item_consumption_services = item_consumption_service_factory.build_list_by_player_id(@player_character.id)
+    @item_consumption_services = @item_consumption_service_factory.build_list_by_player_id(@player_character.id)
   end
 
   def use
     @user_item_id = params[:user_item_id]
 
     @player_character = @player_character_factory.build_by_user_id(current_user.id)
-
     redirect_to '/player/input' if @player_character.nil?
-
-    # ターゲットがあるか
-    trait_factory = TraitFactory.new(@player_character, @soldier_character_facotry)
-    item_consumption_service_factory = ItemConsumptionServiceFactory.new(trait_factory)
 
     # user_itemを取得
     user_item = UserItem.find_by(id: @user_item_id, player_id: @player_character.id)
     fail 'no such user item' + @user_item_id.to_s + 'for player_id: ' + @player_character.id.to_s unless user_item
 
-    item_consumption_service = item_consumption_service_factory.build_by_player_id_and_user_item(@player_character.id, user_item)
-
+    item_consumption_service = @item_consumption_service_factory.build_by_player_id_and_user_item(@player_character.id, user_item)
     @targets = item_consumption_service.targets
   end
 
@@ -39,17 +31,13 @@ class ItemController < ApplicationController
     target_id    = params[:target_id]
 
     @player_character = @player_character_factory.build_by_user_id(current_user.id)
-
     redirect_to '/player/input' if @player_character.nil?
-
-    trait_factory = TraitFactory.new(@player_character, @soldier_character_facotry)
-    item_consumption_service_factory = ItemConsumptionServiceFactory.new(trait_factory)
 
     # user_itemを取得
     user_item = UserItem.find_by(id: @user_item_id, player_id: @player_character.id)
     fail 'no such user item: ' + @user_item_id.to_s + ' for player_id: ' + @player_character.id.to_s unless user_item
 
-    item_consumption_service = item_consumption_service_factory.build_by_player_id_and_user_item(@player_character.id, user_item)
+    item_consumption_service = @item_consumption_service_factory.build_by_player_id_and_user_item(@player_character.id, user_item)
 
     @result = item_consumption_service.use(target_type, target_id)
 
@@ -59,7 +47,8 @@ class ItemController < ApplicationController
   end
 
   def sell
-    shop_id      = params[:shop_id]
+    #TODO: 将来的にはどの店に売ったかを記録してそれを販売するようにする。
+    #shop_id      = params[:shop_id]
     user_item_id = params[:user_item_id]
 
     @player_character = @player_character_factory.build_by_user_id(current_user.id)
@@ -83,6 +72,8 @@ class ItemController < ApplicationController
     equipped_list_service_factory = EquippedListServiceFactory.new(equipped_service_factory)
     @soldier_character_facotry = SoldierCharacterFactory.new
     @player_character_factory = PlayerCharacterFactory.new(equipped_list_service_factory)
+    @trait_factory = TraitFactory.new(@player_character_factory, @soldier_character_facotry)
+    @item_consumption_service_factory = ItemConsumptionServiceFactory.new(@trait_factory)
   end
 end
 
