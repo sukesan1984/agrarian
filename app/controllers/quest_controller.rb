6f2ef6a9fba40b3f87fea773f8cfb11804ca3e01
sukesan1984 <takamikousuke@gmail.com>
2015-07-31 19:21:43 +0900

    quest_entity_factoryリファクタ

--- app/controllers/quest_controller.rb
+++ app/controllers/quest_controller.rb
@@ -5,11 +5,10 @@ class QuestController < ApplicationController
 
   def index
     # quest_factory
-    quest_entity_factory = Quest::QuestEntityFactory.new(@player_character, @quest_condition_entity_factory)
     @user_quests = UserQuest.where(player_id: @player_character.id).select { |user_quest| !user_quest.is_not_received }
     @quest_entities = []
     @user_quests.each do |user_quest|
-      quest_entity = quest_entity_factory.build_by_user_quest(user_quest)
+      quest_entity = @quest_entity_factory.build_by_user_quest_and_player_id(user_quest, @player_character.id)
       @quest_entities.push(quest_entity)
       Quest::QuestAchieveService.new(quest_entity).achieve
     end
@@ -17,7 +16,6 @@ class QuestController < ApplicationController
 
   def claim
     user_quest_id = params[:user_quest_id]
-    quest_entity_factory = Quest::QuestEntityFactory.new(@player_character, @quest_condition_entity_factory)
 
     # 報酬付与のためのitem_serviceのfactory
     item_service_factory = ItemServiceFactory.new(@player_character)
@@ -25,7 +23,7 @@ class QuestController < ApplicationController
     user_quest = UserQuest.find_by(id: user_quest_id, player_id: @player_character.id)
     fail 'user_quest is not found: ' + id.to_s if user_quest.nil?
 
-    quest_entity = quest_entity_factory.build_by_user_quest(user_quest)
+    quest_entity = @quest_entity_factory.build_by_user_quest_and_player_id(user_quest, @player_character.id)
     @item_service = item_service_factory.build_by_gift_id(quest_entity.gift_id)
 
     Quest::QuestClaimService.new(quest_entity, @item_service).claim
@@ -38,6 +36,7 @@ class QuestController < ApplicationController
     equipped_list_service_factory = EquippedListServiceFactory.new(equipped_service_factory)
     @player_character_factory = PlayerCharacterFactory.new(equipped_list_service_factory)
     @quest_condition_entity_factory = Quest::QuestConditionEntityFactory.new
+    @quest_entity_factory = Quest::QuestEntityFactory.new(@player_character_factory, @quest_condition_entity_factory)
   end
 
   def set_player_character
--- app/factories/quest/quest_entity_factory.rb
+++ app/factories/quest/quest_entity_factory.rb
@@ -1,16 +1,18 @@
 class Quest::QuestEntityFactory
-  def initialize(player, quest_condition_entity_factory)
-    @player = player
+  def initialize(player_character_factory, quest_condition_entity_factory)
+    @player_character_factory = player_character_factory
     @quest_condition_entity_factory = quest_condition_entity_factory
   end
 
-  def build_by_user_quest(user_quest)
+  def build_by_user_quest_and_player_id(user_quest, player_id)
+    player = @player_character_factory.build_by_player_id(player_id)
+
     quest_id = user_quest.quest_id
     quest = Quest.find_by(id: quest_id)
     quest_conditions = QuestCondition.where(quest_id: quest_id)
     quest_condition_entities = []
     quest_conditions.each do |quest_condition|
-      quest_condition_entities.push(@quest_condition_entity_factory.build_by_quest_condition(quest_condition, @player))
+      quest_condition_entities.push(@quest_condition_entity_factory.build_by_quest_condition(quest_condition, player))
     end
     return Quest::QuestEntity.new(quest, user_quest, quest_condition_entities)
   end
