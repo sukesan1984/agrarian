67d0602088cdf838a4041702bba58aee105d4ad0
sukesan1984 <takamikousuke@gmail.com>
2015-08-11 23:43:57 +0900

    仲間一覧ページ

---
+++ app/controllers/soldier_controller.rb
@@ -0,0 +1,2 @@
+class SoldierController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,9 @@ Rails.application.routes.draw do
   get 'quest', to: 'quest#index'
   post 'quest/claim', to: 'quest#claim' 
 
+  # soldier
+  get 'soldier', to: 'soldier#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
