32b356adfe9dc38240c9783bfb1478fc0c0a75e4
sukesan1984 <takamikousuke@gmail.com>
2015-08-18 22:34:10 +0900

    skill用のdb schema定義

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816025644) do
+ActiveRecord::Schema.define(version: 20150818133337) do
 
   create_table "area_nodes", force: :cascade do |t|
     t.integer  "area_id",    limit: 4
@@ -268,6 +268,13 @@ ActiveRecord::Schema.define(version: 20150816025644) do
     t.integer  "cost",        limit: 4
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "soldiers", force: :cascade do |t|
---
+++ spec/models/skill_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: skills
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Skill, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
