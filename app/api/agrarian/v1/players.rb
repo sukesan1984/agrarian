8d2bb021e0e1f56f1c3c527ab5466038aacda66b
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-23 16:26:25 +0900

    [modify] resourceを使て記述

--- app/api/agrarian/v1/players.rb
+++ app/api/agrarian/v1/players.rb
@@ -1,21 +1,23 @@
 module Agrarian::V1
   class Players < Grape::API
-    get '/players' do
-      Player.all
-    end
-
-    get '/players/:id' do
-      Player.all.find(params[:id])
-    end
-
-    get '/players/ranking/rails' do
-      players = Player.all.sort_by(&:rails)
-      ranking = []
-      players.each.with_index(1) do |player, k|
-        link = "http://agrarian/players/#{player[:id]}"
-        ranking.push rank: k, id: player[:id], name: player[:name], rails: player[:rails], player: link
+    resource :players do
+      get '' do
+        Player.all
       end
-      ranking
+
+      get ':id' do
+        Player.all.find(params[:id])
+      end
+
+      get 'ranking/rails' do
+        players = Player.all.sort_by(&:rails)
+        ranking = []
+        players.each.with_index(1) do |player, k|
+          link = "http://agrarian/players/#{player[:id]}"
+          ranking.push rank: k, id: player[:id], name: player[:name], rails: player[:rails], player: link
+        end
+        ranking
+      end
     end
   end
 end
