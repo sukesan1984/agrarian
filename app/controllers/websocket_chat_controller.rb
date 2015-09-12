ef3a82016ff74b638310d87813032255a2d74ea8
sukesan1984 <takamikousuke@gmail.com>
2015-09-12 15:34:11 +0900

    一旦controllerを通るようになったのをcommit

--- app/controllers/websocket_chat_controller.rb
+++ app/controllers/websocket_chat_controller.rb
@@ -1,9 +1,19 @@
 class WebsocketChatController < WebsocketRails::BaseController 
   def message_receive 
-    # クライアントからのメッセージを取得
-    #
+　　# クライアントからのメッセージを取得
 　　# websocket_chatイベントで接続しているクライアントにブロードキャスト
-　　# #send_message(:websocket_chat, message()) 
-    WebSocketRails[:websocket_with_channel].trigger(:websocket_chat, message)
+
+　　receive_message = message()
+
+　　broadcast_message(:websocket_chat, receive_message)
+  end
+
+  def message_receive2
+    broadcast_message(:websocket_chat, message())
+  end
+
+  def connect
+    Rails.logger.debug('connected')
+    broadcast_message(:websocket_chat, 'hoge')
   end
 end
--- config/events.rb
+++ config/events.rb
@@ -11,5 +11,7 @@ WebsocketRails::EventMap.describe do
   #     subscribe :new, :to => ProductController, :with_method => :new_product
   #   end
   # The above will handle an event triggered on the client like `product.new`.
-  subscribe :websocket_chat, to: WebsocketChatController, with_method: :message_receive
+  subscribe :websocket_chat, :to => WebsocketChatController, :with_method => :message_receive
+  subscribe :websocket_chat2, :to => WebsocketChatController, :with_method => :message_receive2
+  subscribe :client_connected, :to => WebsocketChatController, :with_method => :connect
 end
