2a62e6e1db54d49544ad18fbc45e799e39ea2628
sukesan1984 <takamikousuke@gmail.com>
2015-10-06 23:43:16 +0900

    chance_to_hitの計算

--- app/policies/damage_calculation.rb
+++ app/policies/damage_calculation.rb
@@ -1,10 +1,24 @@
 class DamageCalculation
-  def initialize(status, target_status)
-    @status = status
-    @target_status = target_status
+  MAX_CHANCE_TO_HIT     = 200
+  MINIMUM_CHANCE_TO_HIT = 20
+  def initialize(attacker_status, defender_status)
+    @attacker_status = attacker_status
+    @defender_status = defender_status
   end
 
   # 攻撃が当たるかどうか
   def dodge?()
+  end
+
+  def get_chance_to_hit
+    chance_to_hit = MAX_CHANCE_TO_HIT * (@attacker_status.attack.fdiv((@attacker_status.attack + @defender_status.defense)))
+    if (chance_to_hit <= MINIMUM_CHANCE_TO_HIT)
+      return MINIMUM_CHANCE_TO_HIT
+    end
+    if (chance_to_hit > 100)
+      chance_to_hit = 100
+    end
+
+    return chance_to_hit - @defender_status.dodge_chance
   end
 end
